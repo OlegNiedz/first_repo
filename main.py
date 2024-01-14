fcb9170fc1082a7f07d41290cdd5da4766375573
OlegNiedz <oleg.niedz@gmail.com>
2024-01-14 17:12:54 +0100

    140124

---
+++ main.py
@@ -0,0 +1,68 @@
+# """ # first_name = "Oleg"
+# # last_name = "Niedzielski"
+# # full_name = first_name + " " + last_name
+# # print(full_name)
+
+# # length = '2.75'
+# # width = '1.75'
+# # area = float(length) * float(width)
+# # show = f"With width {width} and length {length} of the room, its area is equal to {area}"
+# # print(show)
+
+# # name = input("Your name? ",)
+# # email = input("Your email? ")
+# # age = int(input("Your age? "))
+# # height = float(input("Your height? "))
+# # is_active = bool(input("is_active? "))
+# # print(f"name = {name}; email = {email}; age = {age}; height = {height}; is_active = {is_active}")
+
+# # length = float(input("lenght of room? "))
+# # width = float(input("width of room? "))
+# # area = length*width
+# # print(area)
+
+# # my_list = []
+# # my_list.insert(0,2024)
+# # my_list.insert(1,"Pyton")
+# # my_list.insert(2,3.12)
+# # for element_of_my_list in my_list:
+# #     print(element_of_my_list)
+
+# my_list = [2024, 3.12]
+# some_data = ['Python']
+# my_list.extend(some_data)
+# my_list.insert(1,"Python")
+# my_list.reverse()
+
+# for element_of_my_list in my_list:
+#     print(element_of_my_list)
+
+# data = {}
+# data["year"] = 2024
+# data["lang"] = "Python"
+# data["version"] = 3.12
+# print(data)
+
+# cat = {}
+# info = {"status": "vaccinated", "breed": True}
+# cat["nick"] = "Simon"
+# cat["age"] = 7
+# cat["characteristics"] = ["лагідний", "кусається"]
+# age = cat.get("age")
+# cat.update(info)
+
+# print(age)
+# print(cat) """
+
+# a=2
+# print(type(a))
+# a=a/1
+# print(type(a))
+import datetime
+
+# Створення об'єктів date і time
+date_part = datetime.date(24,2,3)
+time_part = datetime.datetime.now()
+
+print(date_part)
+print(time_part)  # Виведе type
